8c4b3f60515ec5b7b8175db4caccb27bd1322aa1
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-05-23 10:15:29 +0800

    Готовая

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+    first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+
+create table customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+
+);
+
+create table orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100),
+	employee_id varchar(100),
+	order_date varchar(100),
+	ship_city varchar(100)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,82 @@
+# os.path.join('north_data', 'customers_data.csv'
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+#импортируем
+import psycopg2, os, csv
+
+
+class BaseClass:
+    def __init__(self):
+        '''инициализиуоем пути и пароль'''
+        self.paswd = input('Введите пароль')
+        self.csv_file = ""
+
+    def get_csv(self):
+        '''универсальный ридер из csv возвращает список'''
+        with open(self.csv_file, encoding='utf-8') as file:
+
+            data = []
+            file_reader = csv.reader(file, delimiter=",")
+            for row in file_reader:
+
+                data.append(row)
+        return data
+
+    def customers(self):
+        '''Метод для customers'''
+        self.csv_file = os.path.join('north_data', 'customers_data.csv')
+        data = self.get_csv()
+
+        with psycopg2.connect(database='north', user='postgres', password=self.paswd) as conn:
+            with conn.cursor() as cur:
+                count = 0
+                for row in data:
+                    if count == 0:# это что  бы название стобцов не лезло в бд
+                        count = 1
+                        continue
+                        # вобще идея у меня была написать один метод для всех таблиц и менять только ф-строку,
+                        # но  я так и не справился нормально с экранированием в sql и питоне,поэтмоу просто написал
+                        # 3 разных метода
+                    comp_name = row[1].replace("'", "''")
+
+                    cur.execute(
+                    f"INSERT INTO customers VALUES ('{row[0]}', '{comp_name}', '{row[2]}');")
+            conn.commit()
+    def employees(self):
+        '''МЕтод для  employees'''
+        self.csv_file = os.path.join('north_data', 'employees_data.csv')
+        data = self.get_csv()
+        count = 0
+        with psycopg2.connect(database='north', user='postgres', password=self.paswd) as conn:
+            with conn.cursor() as cur:
+                for row in data:
+
+                    if count == 0:
+                        count = 1
+                        continue
+
+                    cur.execute(
+                    f"INSERT INTO employees VALUES ('{row[0]}', '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}');")
+            conn.commit()
+    def orders(self):
+        '''Метод для orders'''
+        self.csv_file = os.path.join('north_data', 'orders_data.csv')
+        data = self.get_csv()
+        count = 0
+        with psycopg2.connect(database='north', user='postgres', password=self.paswd) as conn:
+            with conn.cursor() as cur:
+                for row in data:
+
+                    if count == 0:
+                        count = 1
+                        continue
+
+                    cur.execute(
+                        f"INSERT INTO orders VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}');")
+            conn.commit()
+# я хотел что бы синтаксис был типа copy1 = BaseClass(файл,ф-строка), что бы можно было подключать любые csv
+
+copy1 = BaseClass()
+copy1.customers()
+copy1.employees()
+copy1.orders()
+
